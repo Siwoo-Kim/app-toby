ddfa35300939cfc4ebdf8c1f5260a96b4b457718
Siwoo <skim327@myseneca.ca>
2018-03-29 18:23:29 -0400

    first commit

--- src/main/java/com/siwoo/projpa/ProdRootConfig.java
+++ src/main/java/com/siwoo/projpa/ProdRootConfig.java
@@ -1,9 +1,14 @@
 package com.siwoo.projpa;
 
 import com.siwoo.projpa.util.MakeSuore;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
 
 @Profile("prod")
 @Configuration
@@ -14,4 +19,10 @@ public class ProdRootConfig {
         return new MakeSuore("prod");
     }
 
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 }
